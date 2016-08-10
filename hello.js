d74c98c6e60f7559bac752327a012e8df00c58a7
845968074 <845968074@qq.com>
2016-08-10 18:13:35 +0800

    an

--- hello.js
+++ hello.js
@@ -1,31 +1,3 @@
-function () {
+function anfen() {
     
-}
-
-function a() {
-  console.log('hello world!');
-}
-function an()
-{
-console.log("anfen")
-}
-
-function litingting() {
-  console.log('hello world!');
-}
-
-
-
-function xiayu(){
-    console.log('xiayu');
-    console.log('xixi');
-}
-
-function tyb(){
-  console.log("tyb");
-  aaa;
-}
-
-function tyb2() {
-  ddd;
 }
